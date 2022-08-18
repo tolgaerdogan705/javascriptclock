9566d1c571c6535592b04b17a579ab5dc097164b
tolgaerdogan705 <tolga.erdogan705@gmail.com>
2022-08-18 17:37:01 +0300

    son

--- js/clock.js
+++ js/clock.js
@@ -1,22 +1,30 @@
 let kullanici = prompt ("Lütfen Adınızı Giriniz!")
     if (kullanici) {
         document.getElementById("myName").innerHTML = kullanici
-
     }
 
 
+
     function startTime() {
+        const weekday = ["Pazar", "Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
         const today = new Date();
         let h = today.getHours();
         let m = today.getMinutes();
         let s = today.getSeconds();
+        let d =today.getDay();
+        let day = weekday[d];
         m = checkTime(m);
         s = checkTime(s);
-        document.getElementById('myClock').innerHTML =  h + ":" + m + ":" + s;
+        document.getElementById('myClock').innerHTML =  h + ":" + m + ":" + s +"  "+ day;
         setTimeout(startTime, 1000);
+
       }
       
       function checkTime(i) {
         if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
         return i;
       }
+
+   
+      
+    // document.getElementById("myClock").innerHTML= par;
